eb4e5da1204b3ec3a178be49dc3aa3e47f77607a
FrankDestro <frank-destro@outlook.com>
2025-02-07 14:27:38 -0300

    feat : teste de commit

--- Back/src/main/java/com/dev/ServiceHelp/controllers/UserController.java
+++ Back/src/main/java/com/dev/ServiceHelp/controllers/UserController.java
@@ -31,6 +31,7 @@ public class UserController {
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/getAllUsers")
+
     public ResponseEntity<Page<UserDTO>> getUserPaged(
             @RequestParam(name = "id", required = false) Long id,
             @RequestParam(name = "name", defaultValue = "") String name,
